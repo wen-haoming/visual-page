1f93af7e1436b015d05bfe04273a56893cdb832e
wen-haoming <wen-haoming@users.noreply.github.com>
2022-09-05 03:13:30 +0000

    deploy: 070303dfbc80039cfc06c9919f893ebdb8d55875

--- src/LeftPanel.tsx
+++
@@ -1,6 +0,0 @@
-const LeftPanel = ()=>{
-
-  return <div className="w-1/5 mt-1 mr-1 bg-brand-dark">LeftPanel</div>
-}
-
-export default LeftPanel
--- src/RightPanel.tsx
+++
@@ -1,6 +0,0 @@
-const RightPanel = ()=>{
-
-  return <div className="w-1/5 ml-1 mt-1 bg-brand-dark">RightPanel</div>
-}
-
-export default RightPanel
